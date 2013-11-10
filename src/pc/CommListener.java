88fd927cd55f6a0e7564368449d25f17791ac6c6
Corey Short <cshort012@gmail.com>
2013-11-10 14:50:25 -0800

    commlistener builds the gui for the final project. offscreendrawing draws the grid for the gui. it has a bug with the mouse listener and should be allowing the user to click coordinates on the grid

---
+++ src/pc/CommListener.java
@@ -0,0 +1,292 @@
+package pc;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+import java.awt.FlowLayout;
+
+
+
+/**
+ * GUI to draw robot path and current location of the robot. Uses mouse to
+ * enter x,y coordinates and transmits commands from the GUI to the robot.
+ * @author Corey Short, Phuoc Nguyen, Khoa Tran. 
+ * Reference to Glassey OffScreenGrid.java and Milestone 5 sample GUI.
+ */
+public class CommListener extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField nameField, xTextField, yTextField, xTextField2, yTextField2; 
+	private JTextField headingField, movingField, statusField, amountField;
+	private JLabel lblX, lblY, lbl2X, lbl2Y, lblAmount, lblHeading;
+	private JLabel lblData, lblPose, lblMoving, lblStatus;
+	private JButton stopButton, setPoseButton, gotoButton, map1Button, map2Button;
+	private JButton fixButton, travelButton, rotateButton, rotateToButton, pingButton;
+	private JButton connectButton;
+	
+	/**
+	 * provides communications services: sends and receives NXT data
+	 */
+	
+	//private Message communicator = new Message(this);
+	private OffScreenDrawing oSGrid = new OffScreenDrawing();
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CommListener frame = new CommListener();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 * @author Short
+	 */
+	public CommListener() {
+		setTitle("Mission Control");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 650);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		JPanel topPanel = new JPanel();
+		topPanel.setBounds(new Rectangle(0, 0, 200, 50));
+		contentPane.add(topPanel, BorderLayout.NORTH);
+		topPanel.setLayout(new GridLayout(3, 1, 0, 0));
+
+		JPanel connectPanel = new JPanel();
+		topPanel.add(connectPanel);
+		
+		connectButton = new JButton("connect to");
+		connectButton.setBounds(15, 5, 95, 25);
+		connectButton.addActionListener(new BtnConnectActionListener());
+		connectPanel.setLayout(null);
+		connectPanel.add(connectButton);
+
+		nameField = new JTextField();
+		nameField.setBounds(120, 11, 100, 15);
+		nameField.setColumns(10);
+		connectPanel.add(nameField);
+		
+		lblData = new JLabel("Data");
+		lblData.setBounds(245, 0, 30, 15);
+		connectPanel.add(lblData);
+
+		lblX = new JLabel("X");
+		lblX.setBounds(245, 20, 15, 15);
+		connectPanel.add(lblX);
+
+		xTextField = new JTextField();
+		xTextField.setBounds(260, 20, 30, 15);
+		xTextField.setColumns(5);
+		connectPanel.add(xTextField);
+
+		lblY = new JLabel("Y");
+		lblY.setBounds(300, 20, 15, 15);
+		connectPanel.add(lblY);
+
+		yTextField = new JTextField();
+		yTextField.setBounds(315, 20, 30, 15);
+		yTextField.setColumns(5);
+		connectPanel.add(yTextField);
+		
+		lblAmount = new JLabel("Amount");
+		lblAmount.setBounds(360, 20, 50, 15);
+		connectPanel.add(lblAmount);
+		
+		amountField = new JTextField();
+		amountField.setBounds(410, 20, 30, 15);	
+		amountField.setColumns(5);
+		connectPanel.add(amountField);
+		
+		lblPose = new JLabel("Pose");
+		lblPose.setBounds(460, 0, 30, 15);
+		connectPanel.add(lblPose);
+		
+		lbl2X = new JLabel("X");
+		lbl2X.setBounds(460, 20, 15, 15);
+		connectPanel.add(lbl2X);
+		
+		xTextField2 = new JTextField();
+		xTextField2.setBounds(475, 20, 30, 15);
+		xTextField2.setColumns(5);
+		connectPanel.add(xTextField2);
+		
+		lbl2Y = new JLabel("Y");
+		lbl2Y.setBounds(515, 20, 15, 15);
+		connectPanel.add(lbl2Y);
+		
+		yTextField2 = new JTextField();
+		yTextField2.setBounds(530, 20, 30, 15);
+		yTextField2.setColumns(5);
+		connectPanel.add(yTextField2);
+		
+		lblHeading = new JLabel("Heading");
+		lblHeading.setBounds(570, 20, 50, 15);
+		connectPanel.add(lblHeading);
+		
+		headingField = new JTextField();
+		headingField.setBounds(620, 20, 30, 15);
+		headingField.setColumns(5);
+		connectPanel.add(headingField);
+		
+		lblMoving = new JLabel("Moving");
+		lblMoving.setBounds(660, 20, 50, 15);
+		connectPanel.add(lblMoving);
+		
+		movingField = new JTextField();
+		movingField.setBounds(710, 20, 20, 15);
+		movingField.setColumns(5);
+		connectPanel.add(movingField);
+		
+		JPanel enumPanel = new JPanel();
+		topPanel.add(enumPanel);
+
+		stopButton = new JButton("Stop");
+		stopButton.addActionListener(new SendButtonActionListener());
+		enumPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 7));
+		enumPanel.add(stopButton);
+
+		setPoseButton = new JButton("Set Pose");
+		//setPoseButton.addActionListner(new SetPoseButtonActionLIstner());
+		enumPanel.add(setPoseButton);
+		
+		gotoButton = new JButton("GO TO");
+		//gotoButton.addActionListner(new GoToButtonActionListener());
+		enumPanel.add(gotoButton);
+		
+		map1Button = new JButton("<map");
+		//map1Button.addActionListener(new Map1ButtonActionListner());
+		enumPanel.add(map1Button);
+		
+		map2Button = new JButton("map>");
+		//map2Button.addActionListener(new Map2ButtonActionListener());
+		enumPanel.add(map2Button);
+		
+		fixButton = new JButton("Fix");
+		//fixButton.addActionListener(new FixButtonActionListener());
+		enumPanel.add(fixButton);
+		
+		travelButton = new JButton("Travel");
+		//travelButton.addActionListener(new TravelButtonActionListener());
+		enumPanel.add(travelButton);
+		
+		rotateButton = new JButton("Rotate");
+		//rotateButton.addActionListener(new RotateButtonActionListener());
+		enumPanel.add(rotateButton);
+		
+		rotateToButton = new JButton("Rotate To");
+		//rotateToButton.addActionListener(new RotateToButtonActionListener());
+		enumPanel.add(rotateButton);
+		
+		pingButton = new JButton("Ping");
+		//pingButton.addActionListener(new PingButtonActionListener());
+		enumPanel.add(pingButton);
+		
+		JPanel statusPanel = new JPanel();
+		topPanel.add(statusPanel);
+
+		lblStatus = new JLabel("Status");
+		lblStatus.setFont(new Font("Tahoma", Font.BOLD, 13));
+		statusPanel.add(lblStatus);
+
+		statusField = new JTextField();
+		statusPanel.add(statusField);
+		statusField.setColumns(35);
+
+		contentPane.add(oSGrid, BorderLayout.CENTER);
+
+		oSGrid.textX = this.xTextField;
+		oSGrid.textY = this.yTextField;		
+		
+	}
+
+	private class BtnConnectActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			String name = nameField.getText();
+			//communicator.connect(name);
+			System.out.println("Connect to " + name);
+		}
+	}
+	
+	// set pose might look something like this.
+	private class SendButtonActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("send button");
+			int x = 0;
+			int y = 0;
+			try {
+				x = Integer.parseInt(xTextField.getText());
+				System.out.println(" get x " + x);
+			}
+			catch (Exception e) {
+				setInfo("Problem with X field");
+				return;
+			}
+			try {
+				y = Integer.parseInt(yTextField.getText());
+				System.out.println(" get y " + y);
+			}
+			catch (Exception e) {
+				setInfo("Problem with Y field");
+				return;
+			}
+			//communicator.send(x, y);
+			System.out.println("send " + x + " " + y);
+			repaint();
+		}
+	}
+
+	public void setInfo(String message) {
+		statusField.setText(message);
+	}
+
+	public void incomingMessage(int header, int x, int y) {
+		if (header == 0) {
+			oSGrid.drawRobotPath(x, y);
+			System.out.println("Drawing robot path to " + x + " " + y);
+		}
+		if (header == 1) {
+			oSGrid.drawObstacle(x, y);
+			System.out.println("Drawing obstacle path to " + x + " " + y);
+		}
+		System.out.println("HAHAHAHAHA YOU FOOL");
+	}
+
+	
+	public void setMessage(String message) {
+		statusField.setText(message);
+	}
+
+
+	public void drawRobotPath(int x, int y) {
+		incomingMessage(0, x, y);
+	}
+
+
+	public void drawObstacle(int x, int y) {
+		incomingMessage(1, x, y);
+	}
+}
+
--- src/pc/OffScreenDrawing.java
+++ src/pc/OffScreenDrawing.java
@@ -1,205 +1,291 @@
 package pc;
+
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.JPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
 /**
- To avoid maintaing a data record of robot movements, the path is drawn and recorded on the off screen image. <br>
- the paint() method of an Image is does not clear it (unlike the GUI objects)<br> 
- <br>displays updated image on command <br>
- Image coordinates are in pixels ; public method parameters use grid coordinates <br>
- Uses  Image for drawing updating the robot path.
-  by Karthik, modified by R. Glassey Sept 2007  
-  for mars explorer
-*/
+ * OffScreenDrawing.java manages drawing of grid and robot path on an Image 
+ * which is displayed when repaint() is called. 
+ * @author Roger Glassey
+ * @Edit Phuoc Nguyen, Khoa Tran, Corey Short
+ */
+public class OffScreenDrawing extends JPanel implements ActionListener {
+	
+	/**
+	 * The robot path is drawn and updated on this object. <br>
+	 * created by makeImage which is called by paint(); guarantees image always
+	 * exists before used;
+	 */
+	Image offScreenImage;
+	Color color = Color.white;
+	private Graphics2D osGraphics;      // the graphics context of the image. set by makeImage,
+	   							        // used by all methods that draw on the image
+	public final int gridSpacing = 85;  // line spacing in pixels
+	public final int xOrigin = 50;      // x origin in pixels from corner of drawing area
+	public int yOrigin; 			    // y origin in pixels
+	int x, y, nearestX, nearestY;
+	//private int destXo = xpixel(0);
+	//private int destYo = ypixel(0);
+	private int robotPrevX = xpixel(0); // robot position. used by checkContinuity, drawRobotPath
+	private int robotPrevY = ypixel(0); // robot position. used by checkContinuity, drawRobotPath
+	int imageWidth;					    // width of the drawing area.
+								        // set by makeImage, used by clearImage
+	int imageHeight; 				    // height of the drawing area.
+									    // set by makeImage, used by clearImage
+	private boolean block = false;      // node status - true if blocked.
+									    // set by drawObstacle, used by drawRobotPath
+	boolean line = true;
+	private JButton clearButton;
+	public JTextField textX, textY;
+   
+   /**
+    *simple constructor
+    */
+	OffScreenDrawing() {
+		setBackground(Color.white); 
+		System.out.println("OffScreen Drawing constructor");
+		clearButton = new JButton("Clear");
+		add(clearButton);		
+		clearButton.addActionListener(this);
+    }
+   
+	public void init() { // executes after constructor
+		makeImage();
+		addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				formMouseClicked(e);
+			}
+		});
+		
+		addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseMoved(MouseEvent e) {
+				paneMouseMoved(e);
+			}
+		});
+		
+    }
+	
+	private void formMouseClicked(MouseEvent e) {
+		System.out.println("Mouse click event " + e.getX() + "," + e.getY());
+		osGraphics.fillOval(x - 3, y - 3, 10, 10);
+		findNearestCoordinate(x-3, y-3);
+		repaint();		
+	}
 
-public class OffScreenDrawing extends JPanel implements ActionListener
-{
-/**
- *The robot path is drawn and updated on this object. <br>
- *created by makeImage which is called by paint(); guarantees image always exists before used; 
- */
-   Image offScreenImage;  	
-/**
- *width of the dawing area;set by makeImage,used by clearImage
- */  
-   int imageWidth;
-/**
- *height of the dawing are; set by  makeImage,used by clearImage
- */  
-	int imageHeight;  	
- /** 
-  *the graphics context of the image; set by makeImage, used by all methods that draw on the image
-  */
-   private Graphics2D osGraphics;
- /**
- * y origin in pixels
- */
-   private int y0;
-/**
-* line spacing in  pixels
-*/
-   private final int gridSpacing = 50;
-/**
-* origin in pixels from corner of drawing area
-*/
-   private final int origin = 50;
-/**
-  *robot position ; used by checkContinuity, drawRobotPath
-  */  
-   private int robotPrevX = xpixel(0);
-/**
- * robot position; used by checkContinuity, drawRobotPath
- */
-   private int  robotPrevY = ypixel(0); 
+	private void paneMouseMoved(MouseEvent e) {
+		System.out.println("Mouse moved event " + e.getX() + "," + e.getY());
+		Point p = e.getPoint();
+		x = (int) p.getX();
+		y = (int) p.getY();
+	}
 
-/**
- * node status - true if blocked; set by drawObstacle, used by drawRobotPath
- */
-   private boolean block = false;
-/**
- *simple constructor
- */
-   OffScreenDrawing() 
-   {			
-      setBackground(Color.white); 
-      System.out.println(" OffScreen Drawing constructor " );
-      JButton clear = new JButton("Clear");
-      add( clear);		
-      clear.addActionListener(this);
-   }
-   public void init(){ makeImage(); }// executes after constructor
-/**
- * Create the offScreenImage, or make a new one if applet size has changed.
-*/				   
-   public void makeImage() 
-   {	
+	public void actionPerformed(ActionEvent e) {
+		clear();
+	}
+	
+	/**
+	 * Finding nearest coordinates based on Mouse Click. 
+	 * The result will be sent back to XField and YField
+	 * @param x - x coordinate
+	 * @param y - y coordinate
+	 */
+	private void findNearestCoordinate(int x, int y) {
+		double tempVal = 0.0;
+		int thresholdX = 50;
+		int thresholdY = 415;
+		int nearestX = 0, nearestY = 0;
+		
+		nearestX = (x - thresholdX) / 50;
+		tempVal = (x - thresholdX) % 50;
+		System.out.println(tempVal);
+		if (tempVal > 24.0)
+			nearestX += 1;
+		
+		nearestY =  (thresholdY - y) / 50;
+		tempVal =  (thresholdY - y) % 50;
+		System.out.println(tempVal);
+		if (tempVal > 24.0)
+			nearestY += 1;
+		
+		if ((nearestX < 6) && (nearestY < 8) && (nearestX > -1) && (nearestX > -1)) {
+			updateNearestCoordinate(nearestX, nearestY);
+		}
+		else {
+			System.out.println("Out of Bounds!" + nearestX + " " + nearestY);
+			updateErrorCoordinates();
+		}
+	}
+	
+	/**
+	 * When the mouse click is out of bounds, update the Error to XField and YField
+	 */
+	private void updateErrorCoordinates() {
+		textX.setText("Error");
+		textY.setText("Error");
+	}
+	
+	/**
+	 * Update the X,Y coordinate to XField and YField
+	 * @param x
+	 * @param y
+	 */
+	private void updateNearestCoordinate(int x, int y){
+		textX.setText(Integer.toString(x));
+		textY.setText(Integer.toString(y));
+	}
+   
+   /**
+    * Create the offScreenImage, or make a new one if applet size has changed.
+    */				   
+   public void makeImage() {	
+	   	System.out.println("OffScreenDrawing makeImage() called.");
 		imageWidth = getSize().width;// size from the panel
 		imageHeight = getSize().height;
-		y0= imageHeight - origin;
+		yOrigin= imageHeight - xOrigin;
 		robotPrevX = xpixel(0);
 		robotPrevY = ypixel(0);
-		offScreenImage = createImage(imageWidth, imageHeight);// the container can make an image
+		offScreenImage = createImage(imageWidth, imageHeight); // the container can make
+															   // an image
 		osGraphics = (Graphics2D) offScreenImage.getGraphics(); 
 		osGraphics.setColor(getBackground()); 
-		osGraphics.fillRect(0, 0, imageWidth, imageHeight);// erase everything
+		osGraphics.fillRect(0, 0, imageWidth, imageHeight);    // erase everything
 		drawGrid();
    }
-
-	public void actionPerformed(ActionEvent e) { clear(); }
-/**
- *clear the screen and draw a new grid
- */  
-	public void clear()
-	{
+	
+	/**
+	 *clear the screen and draw a new grid
+	 */  
+	public void clear() {
+		System.out.println("Clear buttton pressed.");
 		osGraphics.setColor(getBackground());
-		osGraphics.fillRect(0, 0, imageWidth, imageHeight);// clear the image
+		osGraphics.fillRect(0, 0, imageWidth, imageHeight); // clear the image
 		drawGrid();
 		repaint();
 	}	
-   
-/**
- *Copy off screen canvas to the screen; g is the graphics context of the panel.
- **/
-   public void paintComponent(Graphics g) 
-   {
-	   	super.paintComponent(g);
-	   	if(offScreenImage == null) makeImage();
-		g.drawImage(offScreenImage, 0, 0, this);  //Writes the Image to the screen
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	   	if(offScreenImage == null)
+	   		makeImage();
+		g.drawImage(offScreenImage, 0, 0, this);  // writes the Image to the screen
    }
-/**
- *draws the grid with labels; draw robot at 0,0
- */
-   public void drawGrid()
-	{ 
-		int xmax = 5;
-		int ymax = 7;
-		osGraphics.setColor(Color.green); // Set the line color
-		for(int y = 0; y <= ymax; y ++)
-			 osGraphics.drawLine(xpixel(0),ypixel(y),xpixel(xmax),ypixel(y));//horizontal lines
-		for (int x=0; x<=xmax; x++ )
-			osGraphics.drawLine(xpixel(x),ypixel(0),xpixel(x),ypixel(ymax));// vertical lines
-		osGraphics.setColor(Color.black); //set number color 	
-	   	for(int y=0; y<=ymax;y++) // number the  y axis
-		osGraphics.drawString(y+"", xpixel(-0.5f),ypixel(y)); 
-		for (int x=0; x<=xmax; x++ ) // number the x axis
-		osGraphics.drawString(x+"", xpixel(x),ypixel(-0.5f));
+	
+	/**
+	 *draws the grid with labels; draw robot at 0,0
+	 */
+	public void drawGrid() { 
+		int xmax = 8;
+		int ymax = 4;
+		int numX = -4;
+		osGraphics.setColor(Color.green);	    // Set the line color for the graph
+		for (int y = 0; y <= ymax; y++)
+			 osGraphics.drawLine(xpixel(0),ypixel(y),xpixel(xmax),ypixel(y)); //horizontal
+																			  //lines
+		for (int x = 0; x <= xmax; x++)
+			osGraphics.drawLine(xpixel(x),ypixel(0),xpixel(x),ypixel(ymax)); // vertical
+																			 // lines
+		osGraphics.setColor(Color.black);	    // set number color 	
+		for(int y = 0; y <= ymax; y++)		    // number the  y axis
+			osGraphics.drawString(y * 60 + "", xpixel(-0.3f), ypixel(y)); 
+		for (int x = 0; x <= xmax; x++) {		// number the x axis
+			osGraphics.drawString(numX * 60 + "", xpixel(x), ypixel(-0.2f));
+			numX++;
+		}
 		drawRobotPath(0,0);
+	}
 
-   } 
-
-/**
- *Obstacles shown as magenta dot
- */ 	
-   public void drawObstacle(int x, int y) 
-   {
+	/**
+	 *Obstacles shown as magenta dot
+	 */
+	public void drawObstacle(int x, int y) {
 		x = xpixel(x); // coordinates of intersection
   		y = ypixel(y);
-  		block=true;
+  		block = true;
 		osGraphics.setColor(Color.magenta); 
-		osGraphics.fillOval(x-5,y-5,10,10);//bounding rectangle is 10 x 10
+		osGraphics.fillOval(x-5,y-5,10,10); // bounding rectangle is 10 x 10
 		repaint(100);
-   }
-/**
- *blue line connects current robot position to last position if adjacent to current position
- */  
-  public void drawRobotPath(int xx, int yy) 
-  {     
+    }
+   
+	/**
+	 * blue line connects current robot position to last position if adjacent
+	 * to current position
+	 */
+	public void drawRobotPath(int xx, int yy) {     
 		int x = xpixel(xx); // coordinates of intersection
-  		int y = ypixel(yy);		
-		if(checkContinuity(x, y)) 
-		{
-	  		osGraphics.setColor(Color.blue);
-	  		osGraphics.drawLine(robotPrevX,robotPrevY,x,y);
-		} 
-  	   	if(block)block=false;
-  	   	else	clearRobotOval(robotPrevX, robotPrevY);
-  	   	osGraphics.setColor(Color.blue);
-	  	osGraphics.fillOval(x-5,y-5,10,10);  //show robot position
+		int y = ypixel(yy);		
+		if (checkContinuity(x, y)) {
+			osGraphics.setColor(Color.blue);
+			osGraphics.drawLine(robotPrevX,robotPrevY,x,y);
+		}
+		if (block) 
+			block = false;
+		else
+			clearRobotOval(robotPrevX, robotPrevY);
+		osGraphics.setColor(Color.blue);
+		osGraphics.fillOval(x-5,y-5,10,10);  // show robot position
 	  	robotPrevX = x;
 	  	robotPrevY = y;
 	  	repaint(100);
-  }
-  /**
- *convert grid coordinates to pixels
- */    
-  private  int xpixel(float x)
-  {
-  	 return  origin+(int)(x*gridSpacing);
-  }
-  private int ypixel(float y)
-  {
-   return y0-(int)(y*gridSpacing);
-  }
+	}
+	
+	/**
+	 *convert grid coordinates to pixels
+	 */    
+	private  int xpixel(float x) {
+		return xOrigin + (int) (x * gridSpacing);
+	}
+	
+	/*private int gridX(int xpix) {
+		float x = (xpix - xOrigin) / (1.0f * gridSpacing);
+		return Math.round(x);
+	}*/
+	
+	private int ypixel(float y) {
+		return yOrigin - (int) (y * gridSpacing);
+	}
+	
+	/*private int gridY(int ypix) {
+		float y = (yOrigin - ypix) / (1.0f * gridSpacing);
+		return Math.round(y);
+	}*/
 
- /**
-  * clear the old robot position, arg pixels
-  */ 
-   private void clearRobotOval(int x, int y)
-	{
-  	   osGraphics.setColor(Color.white); 
-	   osGraphics.fillOval(x-5,y-5,10,10);
-	   osGraphics.setColor(Color.blue); 
-	   osGraphics.drawLine(x-5,y,x+5,y);
-	   osGraphics.drawLine(x,y-5,x,y+5);
-  }
- /**
-  *see of robot has moved to adjacent node - used by drawRobotPath
-  */ 
-  private boolean checkContinuity(int x, int y)
-   {
-	  	if( (abs(x-robotPrevX)==0 && abs(y-robotPrevY)==gridSpacing) ||
-	 			(abs(x-robotPrevX)==gridSpacing && abs(y-robotPrevY)==0) || 
-	 			(abs(x-robotPrevX)==0 && abs(y-robotPrevY)==0))
-	  		return true;
-	  	else 
-	  		return false;
-  }
+	/**
+     * clear the old robot position, arg pixels
+     */ 
+	private void clearRobotOval(int x, int y) {
+		System.out.println("Clear robot oval called.");
+  	   	osGraphics.setColor(Color.white); 
+  	   	osGraphics.fillOval(x-5,y-5,10,10);
+  	   	osGraphics.setColor(Color.blue); 
+  	   	osGraphics.drawLine(x-5,y,x+5,y);
+  	   	osGraphics.drawLine(x,y-5,x,y+5);
+	}
+	
+	/**
+	 *see of robot has moved to adjacent node - used by drawRobotPath
+	 */ 
+	private boolean checkContinuity(int x, int y) {
+		if ((abs(x - robotPrevX) == 0 && abs(y - robotPrevY) == gridSpacing)
+				|| (abs(x - robotPrevX) == gridSpacing && abs(y - robotPrevY) == 0)
+				|| (abs(x - robotPrevX) == 0 && abs(y - robotPrevY) == 0)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
   
-  public int abs(int a) {
-  	return (a<0 ? (-a) : (a));
-  }
+  	public int abs(int a) {
+  		return (a<0 ? (-a) : (a));	
+  	}
+  	
                      
 } 
